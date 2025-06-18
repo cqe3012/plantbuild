343b18b2e593451907dc3269330ab1b0a8a94e30
cuiqe <cuiqe1220@163.com>
2025-06-18 14:46:33 +0800

    loginv1

---
+++ Plant1/login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>登录</title>
+    <link type="text/css" href="css/登录.css" rel="stylesheet">
+    <!-- 确保引入了正确的CSS文件 -->
+</head>
+<body>
+<div id="divd">
+    <p id="p1">欢迎来到登录页面！</p>
+    <div class="form-container">
+        <div id="div1">
+            <form action="administrator_handing" method="POST">
+                <label style="font-size:28px;">管理员登录</label><br><br>
+                <label style="font-size:20px;">姓名</label>&nbsp;&nbsp;
+                <input type="text" id="name" name="name2" onFocus="this.style.backgroundColor = '#CF9'" required>
+                <span id="span5"></span><br><br>
+                <label style="font-size:20px;">密码</label>&nbsp;&nbsp;
+                <input type="password" id="password" name="password2" onFocus="this.style.backgroundColor = '#CF9'" required>
+                <span id="span6"></span><br><br>
+                <input id="submit3" type="submit" value="登录" style="font-size:16px;">
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
